b524d06221d3370dcbc821053db250903dc73056
Aryan Chandel <sharmaaryan8894195623@gmail.com>
2025-05-17 14:40:21 +0530

    Initial commit

---
+++ Backend/app.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connectToDb = require('./db/db');
+
+connectToDb();
+
+app.use(cors());
+
+
+app.get('/',(req, res) => {
+    res.send('hello world');
+});
+
+module.exports =app;
---
+++ Backend/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { use } = require('../app');
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type: String,
+            required: true,
+            minlength:[8,'Firstname must be at least 3 character long'],
+
+
+        },
+        lastname:{
+            type: String,
+            required: true,
+            minlength:[8,'Last name must be at least 3 character long'],
+
+
+        },
+    },
+    email:{
+            type: String,
+            required: true,
+    },
+    password:{
+            type: String,
+            required: true,
+            select : false,
+    },
+    socketId: {
+            type : String,
+    }
+})
+
+userSchema.methods.generateAuthToken = async function (){
+    const token  = jwt.sign({ _id: this.id}, process.env.JWT_SECRET)
+    return token;
+}
+
+userSchema.statics.comparePassword = async function (password){
+    return await bcrypt.compare(password, this.password);
+    
+}
+userSchema.statics.hashPassword = async function(password) {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
